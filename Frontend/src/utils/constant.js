7497068077833416e7d9f272e8978647b02328b1
Parth5409 <parthgaikwad5409@gmail.com>
2024-11-23 08:41:59 +0530

    Vercel setup

--- Frontend/src/utils/constant.js
+++ Frontend/src/utils/constant.js
@@ -1,5 +1,5 @@
-export const USER_API_END_POINT="https://seekerscope.onrender.com/api/v1/user";
-export const JOB_API_END_POINT="https://seekerscope.onrender.com/api/v1/job";
-export const APPLICATION_API_END_POINT="https://seekerscope.onrender.com/api/v1/application";
-export const COMPANY_API_END_POINT="hhttps://seekerscope.onrender.com/api/v1/company";
-export const ROADMAP_API_END_POINT="https://seekerscope.onrender.com/api/v1/roadmap";
+export const USER_API_END_POINT="https://seeker-scope2.vercel.app/api/v1/user";
+export const JOB_API_END_POINT="https://seeker-scope2.vercel.app/api/v1/job";
+export const APPLICATION_API_END_POINT="https://seeker-scope2.vercel.app/api/v1/application";
+export const COMPANY_API_END_POINT="https://seeker-scope2.vercel.app/api/v1/company";
+export const ROADMAP_API_END_POINT="https://seeker-scope2.vercel.app/api/v1/roadmap";
